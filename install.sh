64a9787a974c62d86a84de35255fd902c9a600b9
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2023-04-20 05:14:28 +0200

    Adding another font to render icons properly. Updating installer

--- install.sh
+++ install.sh
@@ -2,21 +2,23 @@
 
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install casks and packages from bundle
 brew bundle --file ./Brewfile
 
-# install git
-brew install git
-
+# install git if it doesn't exist
+if ! command -v git &> /dev/null; then
+  brew install git
+fi
 # clone dotfiles
 git clone https://github.com/lcarv/dotfiles.git ~/.dotfiles
 
-# ---------------
-# --Kitty.term---
-# ---------------
 # config kitty
 ln -s ~/.dotfiles/kitty.conf ~/.config/kitty
 # install kitty.terminal
 brew install kitty
 
 # configure zsh
-ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/.zshrc ~
+
+# configure lsd
+ln -s ~/.dotfiles/lsd ~/.config/lsd
