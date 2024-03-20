a1ed60181379d51a78794b91c10072bd24ad5efa
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2024-03-20 22:34:50 +0100

    Adjusting neo-tree

--- nvim/lua/lcarv/plugins/neo-tree.lua
+++ nvim/lua/lcarv/plugins/neo-tree.lua
@@ -20,6 +20,13 @@ return {
         winbar = false,
         statusline = false,
       },
+      filesystem = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      },
       event_handlers = {
         {
           event = "neo_tree_buffer_enter",
