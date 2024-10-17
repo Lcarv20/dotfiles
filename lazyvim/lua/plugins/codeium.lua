6eb42c8d7f15914ae0fc8ccc165ec88f0df493b6
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2024-10-17 22:05:59 +0200

    Omiting codeium from certain filetypes

--- lazyvim/lua/plugins/codeium.lua
+++ lazyvim/lua/plugins/codeium.lua
@@ -1,28 +1,35 @@
 return {
-  "Exafunction/codeium.vim",
-  event = "BufEnter",
-  enabled = false,
-  config = function()
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set("i", "<c-a>", function()
-      return vim.fn["codeium#Accept"]()
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<c-;>", function()
-      return vim.fn["codeium#CycleCompletions"](1)
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<c-,>", function()
-      return vim.fn["codeium#CycleCompletions"](-1)
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<c-e>", function()
-      return vim.fn["codeium#Clear"]()
-    end, { expr = true, silent = true })
-    vim.cmd([[CodeiumDisable]])
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+    enabled = false,
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<c-a>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-;>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-,>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-e>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
+      vim.cmd([[CodeiumDisable]])
 
-    LazyVim.safe_keymap_set(
-      { "n", "x" },
-      "<leader>uA",
-      "<cmd>CodeiumToggle<cr>",
-      { desc = "Toggle Codeium" }
-    )
-  end,
+      LazyVim.safe_keymap_set({ "n", "x" }, "<leader>uA", "<cmd>CodeiumToggle<cr>", { desc = "Toggle Codeium" })
+    end,
+  },
+  {
+    "Exafunction/codeium.nvim",
+    opts = function()
+      vim.g.codeium_filetypes = {
+        ["DressingInput"] = false,
+        ["neo-tree"] = false,
+        ["oil"] = false,
+      }
+    end,
+  },
 }
