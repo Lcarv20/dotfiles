06757f607ebf4d9852734d2d37e9b0e6f2f78316
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2024-10-29 22:24:47 +0100

    Updating kitty font and telescope nvim

--- lazyvim/lua/plugins/smartcolumn.lua
+++ lazyvim/lua/plugins/smartcolumn.lua
@@ -2,7 +2,7 @@ return {
   "m4xshen/smartcolumn.nvim",
   opts = {
     colorcolumn = "80",
-    disabled_filetypes = { "help", "text", "markdown", "codecompanion", "dashboard" },
+    disabled_filetypes = { "help", "text", "markdown", "codecompanion", "dashboard", "lazy" },
     -- custom_colorcolumn = {},
     scope = "file",
   },
--- lazyvim/lua/plugins/telescope.lua
+++ lazyvim/lua/plugins/telescope.lua
@@ -1,3 +1,12 @@
+local no_preview_picker = {
+  theme = "dropdown",
+  previewer = false,
+  layout_config = {
+    anchor = "N", -- Positions the dropdown towards the top
+    prompt_position = "top", -- Moves the prompt to the top of the window
+  },
+}
+
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
@@ -6,5 +15,35 @@ return {
     { "<leader>,", false },
     { "<leader>:", false },
     { "<leader><space>", false },
+  },
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = {
+        horizontal = {
+          width = 0.8,
+          height = 0.8,
+          preview_width = 0.5,
+        },
+        vertical = {
+          width = 0.9,
+          height = 0.9,
+          preview_height = 0.5,
+        },
+        prompt_position = "top",
+        preview_cutoff = 80, -- Switches to vertical layout if window width is below 80 columns
+      },
+    },
+    pickers = {
+      find_files = no_preview_picker,
+      oldfiles = no_preview_picker,
+      buffers = no_preview_picker,
+      git_commits = {
+        theme = "ivy",
+      },
+      git_status = {
+        theme = "ivy",
+      },
+    },
   },
 }
