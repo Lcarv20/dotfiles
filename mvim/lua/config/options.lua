1378ff04d10d1256c00ff185a8d9c6547c408142
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2025-03-03 21:17:30 +0100

    Hopefully I managed to fix the leader delay issues

--- mvim/lua/config/options.lua
+++ mvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = "\\"
 
 vim.g.have_nerd_font = true
 vim.opt.relativenumber = true -- set relative numbered lines
@@ -8,7 +8,6 @@ vim.opt.showcmd = true
 vim.opt.clipboard = ""
 vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
-vim.bo.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 vim.opt.wrap = true
 vim.opt.number = true
@@ -22,8 +21,8 @@ vim.opt.signcolumn = "yes"
 vim.opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
 -- vim.opt.concealcursor = ''
 vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
-vim.opt.updatetime = 50
-vim.opt.timeoutlen = 200 -- this affects the <leader> pressing with other keys
+vim.opt.updatetime = 200
+vim.opt.timeoutlen = 300 -- this affects the <leader> pressing with other keys
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = false -- Enable listchars
@@ -48,5 +47,33 @@ vim.opt.scrolloff = 10
 -- 	"n:block-NCursor,ci:block-NoiceCursor,v:block-VCursor,i-ci-ve:block-ICursor,r-cr-o:block-RCursor,sm:block-ComCursor"
 -- vim.opt.guicursor =
 -- 	"n:block-deusYellow,ci:block-deusPurple,v:block-deusPurple,i-ci-ve:block-deusGreen,r-cr-o:block-deusRed,sm:block-DeusOrange"
-vim.opt.foldmethod = "manual"
 vim.opt.cmdheight = 2
+
+vim.opt.autowrite = true
+vim.opt.jumpoptions = "view"
+vim.opt.inccommand = "nosplit"
+vim.opt.formatoptions = "jcroqlnt"
+vim.opt.laststatus = 3
+vim.opt.linebreak = true
+vim.opt.pumblend = 10
+vim.opt.pumheight = 10
+vim.opt.ruler = false
+vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+vim.shiftround = true
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitkeep = "screen"
+vim.opt.splitright = true
+vim.opt.undolevels = 10000
+vim.opt.virtualedit = "block"
+vim.opt.wildmode = "longest:full,full"
+vim.opt.winminwidth = 5
+
+vim.opt.smoothscroll = true
+vim.opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
+vim.opt.foldmethod = "expr"
+vim.opt.foldtext = ""
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
