0651e324b614e052da7494db72cfeaa7b74321d2
Luís Carvalho <lcarv@noreply.codeberg.org>
2026-06-13 13:25:10 +0200

    Just for safety

--- mvim/lua/plugins/conform.lua
+++ mvim/lua/plugins/conform.lua
@@ -24,6 +24,7 @@ return {
 			typescript = { "prettier", stop_after_first = true },
 			javascriptreact = { "prettier", stop_after_first = true },
 			typescriptreact = { "prettier", stop_after_first = true },
+      rust = { "rustfmt" },
 		},
 	},
 }
